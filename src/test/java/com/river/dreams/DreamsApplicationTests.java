1135d4199248c5b98cdbdd4f658de97108d0211f
RiverRobins <rivergrahamrobins@gmail.com>
2020-10-17 12:20:53 -0500

    init

---
+++ src/test/java/com/river/dreams/DreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.river.dreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
