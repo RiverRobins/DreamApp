a30e0fbeaee0f8cad04be0b546ce6d5ed845796b
RiverRobins <rivergrahamrobins@gmail.com>
2020-10-17 15:59:17 -0500

    added dependencies

---
+++ src/main/java/main/java/com/river/dreams/Controllers/Dreams.java
@@ -0,0 +1,9 @@
+package main.java.com.river.dreams.Controllers;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+public class Dreams {
+}
---
+++ src/main/java/main/java/com/river/dreams/Models/Dream.java
@@ -0,0 +1,4 @@
+package main.java.com.river.dreams.Models;
+
+public class Dream {
+}
